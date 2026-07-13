0ba832a4f66721e890837bf868fa54d98b7c5e57
salmonco <qhdwltn1380@gmail.com>
2026-07-13 21:38:59 +0900

    test(character): 테스트 분할

--- test/core/move_test.gd
+++ test/core/move_test.gd
@@ -1,16 +1,22 @@
 extends GdUnitTestSuite
 
-func test_캐릭터를_방향키로_움직일_수_있다() -> void:
-	var character := Character.new(Vector2i.ZERO)
+var character: Character
 
+func before_test() -> void:
+	character = Character.new(Vector2i.ZERO)
+
+func test_캐릭터가_위쪽_방향으로_움직인다() -> void:
 	character.move(Vector2i.UP)
-	assert_vector(character.position).is_equal(Vector2i(0, -1))
+	assert_vector(character.position).is_equal(Vector2i.UP)
 
-	character.move(Vector2i.RIGHT)
-	assert_vector(character.position).is_equal(Vector2i(1, -1))
-
+func test_캐릭터가_아래쪽_방향으로_움직인다() -> void:
 	character.move(Vector2i.DOWN)
-	assert_vector(character.position).is_equal(Vector2i(1, 0))
+	assert_vector(character.position).is_equal(Vector2i.DOWN)
 
+func test_캐릭터가_왼쪽_방향으로_움직인다() -> void:
 	character.move(Vector2i.LEFT)
-	assert_vector(character.position).is_equal(Vector2i(0, 0))
+	assert_vector(character.position).is_equal(Vector2i.LEFT)
+
+func test_캐릭터가_오른쪽_방향으로_움직인다() -> void:
+	character.move(Vector2i.RIGHT)
+	assert_vector(character.position).is_equal(Vector2i.RIGHT)
