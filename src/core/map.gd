9a1d4c67ff3bd8c5486b0f41d74137d3a54bbddb
salmonco <qhdwltn1380@gmail.com>
2026-07-14 02:35:30 +0900

    refactor(map): pixel 반환값을 정수에서 실수로 변경

--- src/core/map.gd
+++ src/core/map.gd
@@ -2,5 +2,5 @@ class_name Map
 
 const CELL_SIZE := 64
 
-static func to_pixel(grid: Vector2i) -> Vector2i:
-	return grid * CELL_SIZE
+static func to_pixel(grid: Vector2i) -> Vector2:
+	return Vector2(grid) * CELL_SIZE
--- test/core/map_test.gd
+++ test/core/map_test.gd
@@ -2,8 +2,8 @@ extends GdUnitTestSuite
 
 func test_그리드를_픽셀로_변환한다() -> void:
 	var grid := Vector2i(2, 3)
-	assert_vector(Map.to_pixel(grid)).is_equal(Vector2i(128, 192))
+	assert_vector(Map.to_pixel(grid)).is_equal(Vector2(128, 192))
 
 func test_그리드가_원점이면_픽셀도_원점을_반환한다() -> void:
 	var grid := Vector2i.ZERO
-	assert_vector(Map.to_pixel(grid)).is_equal(Vector2i.ZERO)
+	assert_vector(Map.to_pixel(grid)).is_equal(Vector2.ZERO)
