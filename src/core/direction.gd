f103d8d5137958dcbc8021c91354b449b1af963c
salmonco <qhdwltn1380@gmail.com>
2026-07-14 01:37:36 +0900

    test(direction): 테스트 보강

--- src/core/direction.gd
+++ src/core/direction.gd
@@ -1,11 +1,11 @@
 class_name Direction
 
-const _DIRECTION := {
+const _DIRECTION: Dictionary[Key, Vector2i] = {
 	KEY_UP: Vector2i.UP,
 	KEY_DOWN: Vector2i.DOWN,
 	KEY_LEFT: Vector2i.LEFT,
-	KEY_RIGHT: Vector2i.RIGHT	
+	KEY_RIGHT: Vector2i.RIGHT
 }
 
 static func from_key(key: Key) -> Vector2i:
-	return _DIRECTION[key]
+	return _DIRECTION.get(key, Vector2i.ZERO)
--- test/core/direction_test.gd
+++ test/core/direction_test.gd
@@ -1,13 +1,16 @@
 extends GdUnitTestSuite
 
-func test_키보드_위쪽_방향키를_벡터로_변환한다() -> void:
+func test_키보드_위쪽_방향키를_누르면_위쪽_방향을_가리킨다() -> void:
 	assert_vector(Direction.from_key(KEY_UP)).is_equal(Vector2i.UP)
 
-func test_키보드_아래쪽_방향키를_벡터로_변환한다() -> void:
+func test_키보드_아래쪽_방향키를_누르면_아래쪽_방향을_가리킨다() -> void:
 	assert_vector(Direction.from_key(KEY_DOWN)).is_equal(Vector2i.DOWN)
 
-func test_키보드_왼쪽_방향키를_벡터로_변환한다() -> void:
+func test_키보드_왼쪽_방향키를_누르면_왼쪽_방향을_가리킨다() -> void:
 	assert_vector(Direction.from_key(KEY_LEFT)).is_equal(Vector2i.LEFT)
 
-func test_키보드_오른_방향키를_벡터로_변환한다() -> void:
+func test_키보드_오른쪽_방향키를_누르면_오른쪽_방향을_가리킨다() -> void:
 	assert_vector(Direction.from_key(KEY_RIGHT)).is_equal(Vector2i.RIGHT)
+
+func test_방향키가_아닌_키를_누르면_방향을_가리키지_않는다() -> void:
+	assert_vector(Direction.from_key(KEY_SPACE)).is_equal(Vector2i.ZERO)
