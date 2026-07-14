0029f393199b25992c06931dbd804ad49643c496
salmonco <qhdwltn1380@gmail.com>
2026-07-14 22:38:14 +0900

    feat(water_balloon): 물풍선 터지기

--- src/core/water_balloon.gd
+++ src/core/water_balloon.gd
@@ -1,7 +1,14 @@
 class_name WaterBalloon
 extends RefCounted
 
+const POP_AFTER_SECONDS := 3.0
+
 var position: Vector2i
+var _elapsed_seconds: float
 
 func _init(placed_position: Vector2i) -> void:
 	position = placed_position
+
+func tick(delta: float) -> bool:
+	_elapsed_seconds += delta
+	return _elapsed_seconds >= POP_AFTER_SECONDS
---
+++ test/core/water_balloon_test.gd
@@ -0,0 +1,17 @@
+extends GdUnitTestSuite
+
+func test_물풍선은_놓자마자_터지지_않는다() -> void:
+	var water_balloon := WaterBalloon.new(Vector2i(3, 5))
+	var is_popped := water_balloon.tick(0.1)
+	assert_bool(is_popped).is_false()
+
+func test_물풍선은_시간이_다_지나면_터진다() -> void:
+	var water_balloon := WaterBalloon.new(Vector2i(3, 5))
+	var is_popped := water_balloon.tick(5)
+	assert_bool(is_popped).is_true()
+
+func test_물풍선은_나눠_흐른_시간을_누적한다() -> void:
+	var water_balloon := WaterBalloon.new(Vector2i(3, 5))
+	water_balloon.tick(2.0)
+	var is_popped := water_balloon.tick(2.0)
+	assert_bool(is_popped).is_true()
