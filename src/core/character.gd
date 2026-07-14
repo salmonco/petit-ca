039fc27b44f582cc6a5699069f2d21d556839586
salmonco <qhdwltn1380@gmail.com>
2026-07-14 17:21:28 +0900

    feat(character): 맵 이동 제한

--- src/core/character.gd
+++ src/core/character.gd
@@ -7,6 +7,5 @@ func _init(start_position: Vector2i) -> void:
 	position = start_position
 
 func move(direction: Vector2i) -> void:
-	position = position + direction
-
-
+	var new_position := position + direction
+	position = new_position.clamp(Vector2i.ZERO, Map.GRID_SIZE - Vector2i.ONE)
--- test/core/character_test.gd
+++ test/core/character_test.gd
@@ -1,22 +1,44 @@
 extends GdUnitTestSuite
 
-var character: Character
-
-func before_test() -> void:
-	character = Character.new(Vector2i.ZERO)
-
 func test_캐릭터가_위쪽_방향으로_움직인다() -> void:
+	var character := Character.new(Vector2i.ONE)
 	character.move(Vector2i.UP)
-	assert_vector(character.position).is_equal(Vector2i.UP)
+	assert_vector(character.position).is_equal(Vector2i(1, 0))
 
 func test_캐릭터가_아래쪽_방향으로_움직인다() -> void:
+	var character := Character.new(Vector2i.ONE)
 	character.move(Vector2i.DOWN)
-	assert_vector(character.position).is_equal(Vector2i.DOWN)
+	assert_vector(character.position).is_equal(Vector2i(1, 2))
 
 func test_캐릭터가_왼쪽_방향으로_움직인다() -> void:
+	var character := Character.new(Vector2i.ONE)
 	character.move(Vector2i.LEFT)
-	assert_vector(character.position).is_equal(Vector2i.LEFT)
+	assert_vector(character.position).is_equal(Vector2i(0, 1))
 
 func test_캐릭터가_오른쪽_방향으로_움직인다() -> void:
+	var character := Character.new(Vector2i.ONE)
 	character.move(Vector2i.RIGHT)
-	assert_vector(character.position).is_equal(Vector2i.RIGHT)
+	assert_vector(character.position).is_equal(Vector2i(2, 1))
+
+# 맵 밖으로 이동 제한
+func test_캐릭터는_위쪽_맵_밖으로_이동하지_못한다() -> void:
+	var character := Character.new(Vector2i(1, 0))
+	character.move(Vector2i.UP)
+	assert_vector(character.position).is_equal(Vector2i(1, 0))
+
+func test_캐릭터는_아래쪽_맵_밖으로_이동하지_못한다() -> void:
+	var bottom_row := Map.GRID_SIZE.y - 1
+	var character := Character.new(Vector2i(1, bottom_row))
+	character.move(Vector2i.DOWN)
+	assert_vector(character.position).is_equal(Vector2i(1, bottom_row))
+
+func test_캐릭터는_왼쪽_맵_밖으로_이동하지_못한다() -> void:
+	var character := Character.new(Vector2i(0, 1))
+	character.move(Vector2i.LEFT)
+	assert_vector(character.position).is_equal(Vector2i(0, 1))
+
+func test_캐릭터는_오른쪽_맵_밖으로_이동하지_못한다() -> void:
+	var right_column := Map.GRID_SIZE.x - 1
+	var character := Character.new(Vector2i(right_column, 1))
+	character.move(Vector2i.RIGHT)
+	assert_vector(character.position).is_equal(Vector2i(right_column, 1))
