a632fd4cb819249622586b2e40185c83ed2c8c49
salmonco <qhdwltn1380@gmail.com>
2026-07-22 14:07:50 +0900

    refactor(map): 맵 픽셀 상수 사용

--- test/core/map_test.gd
+++ test/core/map_test.gd
@@ -7,7 +7,7 @@ func before_test() -> void:
 
 func test_그리드를_픽셀로_변환한다() -> void:
 	var grid := Vector2i(2, 3)
-	assert_vector(Map.to_pixel(grid)).is_equal(Vector2(128, 192))
+	assert_vector(Map.to_pixel(grid)).is_equal(Vector2(2 * Map.PIXELS_PER_CELL, 3 * Map.PIXELS_PER_CELL))
 
 func test_그리드가_원점이면_픽셀도_원점을_반환한다() -> void:
 	var grid := Vector2i.ZERO
