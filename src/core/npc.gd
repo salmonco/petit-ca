95418be74110f58187f7ade90d42d80b95a65b0c
salmonco <qhdwltn1380@gmail.com>
2026-07-22 18:50:05 +0900

    feat(npc): NPC도 맵에 넣기

--- src/core/npc.gd
+++ src/core/npc.gd
@@ -2,7 +2,11 @@ class_name Npc
 extends Character
 
 func decide_move_direction(map: Map) -> Vector2i:
-	var character_position := map.characters()[0].continuous_position
+	var target: Character
+	for character in map.characters():
+		if character != self:
+			target = character
+	var character_position := target.continuous_position
 	var delta_x: float = continuous_position.x - character_position.x
 	var delta_y: float = continuous_position.y - character_position.y
 	if abs(delta_x) > abs(delta_y):
